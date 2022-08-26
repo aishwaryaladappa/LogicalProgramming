cd26ab88d2df1c0399905111a7229c35ab004ac9
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-26 13:01:51 +0530

    added PrimeNumber.cs

---
+++ PrimeNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace LogicalProgramming
+{
+    public static class PrimeNumber
+    {
+        public static void DisplayPrimeNumber()
+        {
+            bool isPrime = true;
+            int i, j;
+            //Calculate and display the Prime number  
+            Console.WriteLine("Prime Numbers are : ");
+            for (i = 2; i <= 100; i++)
+            {
+                for (j = 2; j <= 100; j++)
+                {
+                    if (i != 0j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write("\t" + i);
+                }
+                isPrime = true;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
