07ace913abd7f57c02a848ae4a89e0b4b73bfb4d
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-08-31 17:55:55 +0530

    added StopWatch.cs

---
+++ StopWatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+namespace LogicalProgramming
+{
+    public class Stopwatches
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Stopwatches"/> class.
+        /// </summary>
+        public Stopwatches()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+        validation1:
+            Console.WriteLine("To start the stopwatch enter S or s.");
+            char start = Convert.ToChar(Console.ReadLine());
+            stopwatch.Start();
+            if (start == 's' || start == 'S')
+            {
+            validation2:
+                Console.WriteLine("To stop the stopwatch enter Q or q.");
+                char end = Convert.ToChar(Console.ReadLine());
+                if (end == 'q' || end == 'Q')
+                {
+                    stopwatch.Stop();
+                    Console.WriteLine("stopwatch stopped, the time:" + stopwatch.Elapsed);
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Input");
+                    goto validation2;
+                }
+            }
+            else
+            {
+                stopwatch.Stop();
+                Console.WriteLine("Wrong Input");
+                goto validation1;
+            }
+        }
+    }
+}
